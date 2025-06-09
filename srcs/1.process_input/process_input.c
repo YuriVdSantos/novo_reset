c8bf4bb25c8a8e2c4d2ddfc70870ad2f678b6af4
Jhulia Alves De Carvalho <jhualves@c1r8p1.42sp.org.br>
2025-06-09 17:34:05 -0300

    verificações sendo feitas

--- srcs/1.process_input/process_input.c
+++ srcs/1.process_input/process_input.c
@@ -6,11 +6,13 @@
 /*   By: jhualves <jhualves@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 18:54:45 by jhualves          #+#    #+#             */
-/*   Updated: 2025/06/09 16:38:35 by jhualves         ###   ########.fr       */
+/*   Updated: 2025/06/09 17:31:12 by jhualves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	execute_cmd(t_cmd *cmd, t_ctx *ctx);
 
 void	process_input(t_ctx *ctx, const char **input)
 {
@@ -26,18 +28,23 @@ void	process_input(t_ctx *ctx, const char **input)
 	}
 	if (!validate_syntax(ctx, tokens))
 	{
-		ctx->exit_status = 2;
+		ctx->exit_status = 258;
 		return ;
 	}
 	ast = parse_tokens(ctx, &tokens);
 	ctx->cmd_list = ast;
 	if (!ast)
 	{
-		ctx->exit_status = 2;
+		ctx->exit_status = 258;
 		return ;
 	}
+	return (execute_cmd(ast, ctx));
+}
+
+static void	execute_cmd(t_cmd *cmd, t_ctx *ctx)
+{
 	if (ctx->cmd_list && ctx->cmd_list->next)
-		ctx->exit_status = execute_multiple_commands(ast, ctx);
+		ctx->exit_status = execute_multiple_commands(cmd, ctx);
 	else
-		ctx->exit_status = execute_one_command(ast, ctx);
+		ctx->exit_status = execute_one_command(cmd, ctx);
 }
